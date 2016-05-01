e43130257670cc3e81c867cd299f3943542fcaf8
Bryan Hoekstra <hooky@qrmaster.co.nz>
2016-05-02 09:46:05 +1200

    day 1 complete

---
+++ spec/day1_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../1/app/day1.rb'
+RSpec.describe 'Day 1' do
+  context 'Examples' do
+    {
+      '(())': 0, 
+      '()()':0,
+      '(((': 3, 
+      '(()(()(': 3,
+      '))(((((': 3,
+      '())': -1, 
+      '))(': -1,
+      ')))':-3,
+      ')())())': -3
+    }.each do |floor_string, expected_result|
+    
+      context "with example input '#{floor_string}'" do
+        it "results in #{expected_result}" do
+          expect(Day1.new(floor_string.to_s).call).to eq(expected_result)
+        end
+      end
+    end
+  end
+end
