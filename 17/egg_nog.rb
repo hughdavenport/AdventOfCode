4ba6f51dee6dc2c2fca3f39b20dcacf6916f1746
Bryan Hoekstra <hooky@qrmaster.co.nz>
2016-05-03 10:53:54 +1200

    part 17 phase 2

--- 17/egg_nog.rb
+++ 17/egg_nog.rb
@@ -10,12 +10,17 @@ if __FILE__ == $0
     end
   end
   volume_match = 0
+  has_matched = false
   (1..volumes.length).each do |container_count|
     
     volumes.combination(container_count).each do |containers|
       total_volume_of_containers = containers.reduce(:+)
       volume_match +=1 if total_volume_of_containers == total_volume
     end
+    if !has_matched && volume_match > 0
+      puts "Total of minimum container count matches: #{volume_match}"
+      has_matched = true
+    end 
   end
   puts "Total matching combos: #{volume_match}"
 end
