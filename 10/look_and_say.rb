b3684e404d012e73064b360061bab6c7101ea1a9
Bryan Hoekstra <hooky@qrmaster.co.nz>
2016-05-02 16:47:34 +1200

    phase 2 of day 10

--- 10/look_and_say.rb
+++ 10/look_and_say.rb
@@ -1,45 +1,34 @@
 class LookAndSayGenerator
-  def initialize(number = 1)
-    @number = number
+  attr_reader :string
+  
+  def initialize(string = '1')
+    @string = string
   end
-
-  attr_reader :number
-
-  def next
-    new_number = ""
-    grouped_digits(@number).each do |digit_group|
-      new_number += digit_group.length.to_s
-      new_number += digit_group.first.to_s
+  
+  def call
+    result = ''
+    active_char = nil
+    active_count = 0
+    @string.each_char.with_index do |char, index|
+      if index == 0 || active_char != char
+        result << active_count.to_s + active_char if index > 0
+        active_char = char
+        active_count = 1
+      else
+        active_count += 1
+      end
     end
-    @number = new_number.to_i
+    result << active_count.to_s + active_char
   end
-end
-
-def digits(number)
-  number.to_s.chars.map(&:to_i)
-end
-
-def grouped_digits(number)
-  all_digit_groups = []
-
-  digits_group = []
-  digits(number).each do |digit|
-    if digits_group.empty? || digits_group.last == digit
-      digits_group << digit
-    else
-      all_digit_groups << digits_group
-      digits_group = [digit]
-    end
-  end
-
-  all_digit_groups << digits_group unless digits_group.empty?
-
-  all_digit_groups
 end
 
 if __FILE__ == $0
-  input_number = 1321131112
-  generator = LookAndSayGenerator.new(input_number)
-  40.times { |num| generator.next; printf("\rCalculating... %d / 40", num); }
-  printf("\r%d\n", generator.number.to_s.length)
+  input_string = '1321131112'
+  generator = LookAndSayGenerator.new(input_string)
+  
+  50.times do |num| 
+    generator = LookAndSayGenerator.new(generator.call) 
+    printf("\rCalculating... %d / 40", num)
+  end
+  printf("\r\n%d\n", generator.string.length)
 end
--- 10/spec/look_and_say_spec.rb
+++ 10/spec/look_and_say_spec.rb
@@ -2,27 +2,18 @@ require 'rspec'
 require_relative '../look_and_say'
 
 RSpec.describe LookAndSayGenerator do
-  it "initializes to default number" do
+  it "initializes to default string" do
     generator = LookAndSayGenerator.new
-    expect(generator.number).to eq(1)
+    expect(generator.string).to eq('1')
   end
 
   it "generates next number of sequence" do
-    generator = LookAndSayGenerator.new(111221)
-    generator.next
-    expect(generator.number).to eq(312211)
+    generator = LookAndSayGenerator.new('111221')
+    expect(generator.call).to eq('312211')
   end
-end
-
-RSpec.describe '#digits' do
-  it "returns digits of number" do
-    expect(digits(123456)).to eq([1, 2, 3, 4, 5, 6])
-  end
-end
-
-RSpec.describe '#grouped_digits' do
-  it "returns grouped digits of number" do
-    expect(grouped_digits(122333)).to eq([[1], [2, 2], [3, 3, 3]])
-    expect(grouped_digits(111223)).to eq([[1, 1, 1], [2, 2], [3]])
+  
+  it "provides the input" do
+    generator = LookAndSayGenerator.new('111221')
+    expect(generator.string).to eq('111221')
   end
 end
