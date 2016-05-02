828a61a18e35e3fdc34df264920235beb19cff27
Bryan Hoekstra <hooky@qrmaster.co.nz>
2016-05-03 10:50:55 +1200

    17 part 1

---
+++ 17/egg_nog.rb
@@ -0,0 +1,21 @@
+
+total_volume = 150
+if __FILE__ == $0
+
+  volumes = []
+  input_file_name = ARGV[0]
+  File.open(input_file_name) do |file|
+    file.each_line do |line|
+      volumes.push(line.to_i)
+    end
+  end
+  volume_match = 0
+  (1..volumes.length).each do |container_count|
+    
+    volumes.combination(container_count).each do |containers|
+      total_volume_of_containers = containers.reduce(:+)
+      volume_match +=1 if total_volume_of_containers == total_volume
+    end
+  end
+  puts "Total matching combos: #{volume_match}"
+end
