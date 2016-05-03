4b245a67e1ca1513cca8e95d9fd319d01a4c754e
Bryan Hoekstra <hooky@qrmaster.co.nz>
2016-05-03 14:39:13 +1200

    20 part 1

--- 20/presents.rb
+++ 20/presents.rb
@@ -1,11 +1,23 @@
 num = ARGV[0].to_i
 
+def get_factors(num)
+  val = sqrt(num)
+  while val < num
+    
+  end
+end
+
+
 (1..num).each do |house_number|
   count = 0
-  (1..house_number).each do |elf_number|
-    count += elf_number * 10 if house_number % elf_number == 0
+  (1..Math.sqrt(house_number).to_i).each do |elf_number|
+    if house_number % elf_number == 0
+      count += elf_number
+      count += house_number/elf_number if elf_number != Math.sqrt(house_number)
+    end
   end
-  puts "House #{house_number} has count: #{count}"
+  count *= 10
+  puts "House #{house_number} has count: #{count}" if (house_number % 10000) == 0
   if count >= num
     puts "Lowest house number is #{house_number}"
     break
